9f914e33015a99d8b78e8ae1ca40ba4182b14d52
henry eze <henryeze019@gmail.com>
2020-09-07 12:16:38 +0100

    first try with travis

--- tests/unit/task.spec.js
+++ tests/unit/task.spec.js
@@ -35,7 +35,7 @@ describe("task.vue", () => {
         expect(wrapper.find('.status').text()).to.equal('Complete')
       }
   })
-  it('delete should work',async () => {
+  it('delete shoul work',async () => {
    await wrapper.find('button').trigger('click')
     //  find returns the wrapper of the first node matching the selector
     //  findall returns all the wrappers of the node matching the selector
